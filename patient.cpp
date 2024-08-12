75a50f3754c51ea94350aba6c1c1223167d573fa
Ryan James <rcjames1004@gmail.com>
2024-08-12 15:28:27 -0700

    Clean

--- patient.cpp
+++ patient.cpp
@@ -1,6 +1,4 @@
 #include <cstdlib>  // for system()
-#include <cerrno>   // For perror()
-#include <iostream> // For std::cerr
 
 int main() {
     system("C:\\Users\\Dopl\\Downloads\\Dopl\\DoplOR-win32-x64\\DoplOR.exe");
