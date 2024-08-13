ec2cb896fa8f9fa965c9909bd7fed2b89d084180
RemyFrederick <70980007+RemyFrederick@users.noreply.github.com>
2024-08-13 13:17:05 -0700

    Necessary Comments

--- patient.cpp
+++ patient.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>  // for system()
 
 int main() {
+    //TODO: Make this a relative path
     system("C:\\Users\\Dopl\\Downloads\\Dopl\\DoplOR-win32-x64\\DoplOR.exe");
     return 0;
 }
